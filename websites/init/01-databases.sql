1b625d26b3a2390e918341f5c7ed7e21c980fe6c
h3llo0p3rator <thomas@notsosecure.com>
2023-02-14 06:56:33 +0000

    Removed leaked creds
    Someone told us that we leaked passwords in this repo - whoops!
    
    Re-committed to remove leaked passwords

--- websites/init/01-databases.sql
+++ websites/init/01-databases.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS `wordpress`;
 CREATE DATABASE IF NOT EXISTS `blog`;
 
 #create user
-CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'N0tS0H@ck@b13'; --We know this is here
+CREATE USER 'wordpress'@'localhost' IDENTIFIED BY '<insert-password-here>';
 GRANT ALL PRIVILEGES ON *.* TO 'wordpress'@'%';
 FLUSH PRIVILEGES;
